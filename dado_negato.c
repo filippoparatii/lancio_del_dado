1e8a053ae6403788182affa501535e8c7f3058d2
filippoparatii <113536546+filippoparatii@users.noreply.github.com>
2022-09-19 20:27:50 +0200

    Update dado_negato.c

--- dado_negato.c
+++ dado_negato.c
@@ -1,1 +1,33 @@
-nego il dado
+Ora puoi pure piangere se non ci sentiamo
+Ora non mi dispiace se non mi cercherai, ehi
+Resti nel locale col telefono in mano
+Sperando in un messaggio che non arriva mai
+Sono una rockstar, rockstar
+A uccidermi no, non sarà una stronza, ehi
+Il mio cuore è freddo, anche più del mio polso
+E se provi a scaldarlo rischi che si sciolga
+Pusher sul mio iPhone
+Pute sul mio iPad
+Mamma, guarda, senza mani, sono una rockstar
+Mamma, sai che a parte te non amo nessun'altra
+Non esco più di tre volte con una ragazza
+Non innamorarti mai di me (uh)
+Non potrò mai essere il tuo boyfriend, no (uh)
+Fumo dentro la stanza d'hotel
+Chiamano dalla reception
+C'era un ragazzo che come me
+Amava i Beatles e i Rolling Stones
+Metteva kush nelle Rolling Papers
+E lo sciroppo nel biberon (uh)
+Rockstar, rockstar
+Due tipe nel letto e le altre due di là
+Gli amici selvaggi, tutti dentro il privée
+Fanculo il Moët, prendiamo tutto il bar
+E ora puoi pure piangere se non ci sentiamo
+Ora non mi dispiace se non mi cercherai, ehi
+Persi nel locale col telefono in mano
+Sperando in un messaggio che non arriva mai
+Sono una rockstar, rockstar
+Rockstar, rockstar
+Sono una rockstar, rockstar
+Rockstar, rockstar
